99aa570a6af059ee31145dee24eb1bea51c7a15f
dreamerchandra <17010408+dreamerchandra@users.noreply.github.com>
2021-03-31 17:40:30 +0530

    mutation done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import graphql from 'babel-plugin-relay/macro';
 import {
   loadQuery,
   usePreloadedQuery,
+  useMutation
 } from 'react-relay/hooks';
 import RelayEnvironment from './RelayEnvironment';
 import List from './List'
@@ -45,15 +46,44 @@ const FrontEndNameQuery = graphql`
 const backEndPrefetcher = loadQuery(RelayEnvironment, BackEndNameQuery);
 const frontEndPreFetcher = loadQuery(RelayEnvironment, FrontEndNameQuery);
 
+const FRONT_END_APP_ID = "1";
+const BACK_END_APP_ID = "2";
+
 function App() {
   const backEndSnap = usePreloadedQuery(BackEndNameQuery, backEndPrefetcher);
   const backEndSkills = backEndSnap.backEnd ? backEndSnap.backEnd.skills.edges : []
+  
   const frontEndSnap = usePreloadedQuery(FrontEndNameQuery, frontEndPreFetcher);
   const frontEndSkills = frontEndSnap.frontEnd ? frontEndSnap.frontEnd.skills.edges : []
+
+  const [commit, isInFlight] = useMutation(graphql`
+    mutation AppFrontEndMutation($input: IntroduceSkillInput!) {
+      introduceSkill(input: $input) {
+        skill {
+          name
+          id
+        }
+      }
+    }
+  `);
   return (
     <div className="App">
-      <List items={backEndSkills} onNewItem={(newVal) => console.log(newVal)}/>
-      <List items={frontEndSkills} onNewItem={(newVal) => console.log(newVal)}/>
+      <List items={backEndSkills} onNewItem={(skillName) => commit({
+        variables: {
+          input: {
+            skillName,
+            areaId: BACK_END_APP_ID
+          }
+        }
+      })}/>
+      <List items={frontEndSkills} onNewItem={(skillName) => commit({
+        variables: {
+          input: {
+            skillName,
+            areaId: FRONT_END_APP_ID
+          }
+        }
+      })}/>
     </div>
   );
 }
